4b58fce3297b8c23d6b60965c5aa7a8342f23f8d
Krishna <johndoe@example.com>
2023-03-09 01:19:47 +1100

    motion animatiosns

--- app/Components/Section.js
+++ app/Components/Section.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { FaPlay } from "react-icons/fa";
+"use client";
+import React, { useState, useEffect } from "react";
+import { FaArrowLeft, FaArrowRight, FaPlay } from "react-icons/fa";
 import Button from "./Button";
 import "./Section.css";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Section = () => {
   const curosalItems = [
@@ -22,7 +24,7 @@ const Section = () => {
         title: "The Zone",
 
         subHeading: "A specific collection",
-        des: "A quick brown fox jump over the lazy dog",
+        des: "In this example, the curosal-des-animate class is added to the curosal-des element when the slide state changes. This class applies a transition property to the opacity CSS property, which causes the text element to fade in and out smoothly. The setTimeout function is used to remove the class after 1 second, which triggers the animation when the des text changes.",
       },
     },
     {
@@ -36,7 +38,32 @@ const Section = () => {
       },
     },
   ];
-  console.log(curosalItems);
+  // console.log(curosalItems);
+  const [slide, setSlide] = useState(0);
+  const [direction, setDirection] = useState(false);
+
+  const sliderControl = (control) => {
+    const newSlide =
+      (slide + control + curosalItems.length) % curosalItems.length;
+    setSlide(newSlide);
+  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      sliderControl(1);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [slide]);
+
+  useEffect(() => {
+    const heading = document.querySelector(".curosal-des-heading");
+    heading.classList.add("slide-in");
+
+    return () => {
+      heading.classList.remove("slide-in");
+    };
+  }, [slide]);
+
   return (
     <div className="sectionContainer">
       <div className="announcements">
@@ -45,21 +72,51 @@ const Section = () => {
         <Button text={"Play Now"} icons={<FaPlay />} />
       </div>
       <div className="curosal-Conatiner">
-        {curosalItems.map((item, x) => {
-          console.log(item.rightSide);
-          return (
-            <div key={x} className="curosal-Items">
+        <div className="curosal">
+          <div className="curosal-Items">
+            <AnimatePresence wait>
               <div className="imageContainer">
-                <img src={item.image} alt={item.image} />
+                <motion.img
+                  key={curosalItems[slide].image}
+                  initial={{ x: "-100%", opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: "-100%" }}
+                  transition={{ duration: 0.5 }}
+                  src={curosalItems[slide].image}
+                  alt={curosalItems[slide].image}
+                  width={"100%"}
+                  style={{ objectFit: "cover", height: "100%" }}
+                />
               </div>
-              <div className="curosal-des">
-                <h2>{item.rightSide.title}</h2>
-                <h3>{item.rightSide.subHeading}</h3>
-                <span>{item.rightSide.des}</span>
-              </div>
+            </AnimatePresence>
+
+            <div className="curosal-des">
+              <AnimatePresence wait>
+                <motion.h2
+                  className="curosal-des-heading"
+                  key={curosalItems[slide].rightSide.title}
+                  initial={{ x: "100%", opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: "100%" }}
+                  transition={{ duration: 1 }}
+                >
+                  {curosalItems[slide].rightSide.title}
+                </motion.h2>
+                <h3>{curosalItems[slide].rightSide.subHeading}</h3>
+                <span>{curosalItems[slide].rightSide.des}</span>
+              </AnimatePresence>
             </div>
-          );
-        })}
+          </div>
+          <div className="curosalControls">
+            <FaArrowLeft
+              onClick={() => {
+                sliderControl(-1);
+                setDirection(!direction);
+              }}
+            />
+            <FaArrowRight onClick={() => sliderControl(+1)} />
+          </div>
+        </div>
       </div>
     </div>
   );
