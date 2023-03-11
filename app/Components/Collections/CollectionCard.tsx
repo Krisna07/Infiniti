ac7ff28146b9e71e2636cfd378e75bec35c82691
Krishna <krisnachhetri07@gmail.com>
2023-03-11 19:20:52 +1100

    collectionlists complete

--- app/Components/Collections/CollectionCard.tsx
+++ app/Components/Collections/CollectionCard.tsx
@@ -1,43 +1,53 @@
-import React from "react";
-interface collectionItems {
-  imageUrl: String;
-  title: String;
-  collectionCount: String;
+import React, { useState } from "react";
+import { BsStars } from "react-icons/bs";
+import Button from "../Button";
+import { AnimatePresence, motion } from "framer-motion";
+
+interface CollectionItems {
+  imageUrl: string;
+  title: string;
+  collectionCount: string;
 }
 
 const CollectionCard = ({
   imageUrl,
   title,
   collectionCount,
-}: collectionItems) => {
+}: CollectionItems) => {
+  const [browserBtn, showBrowseBtn] = useState(false);
+
   return (
-    <div className="CollectionCardDiv">
+    <div
+      className="CollectionCardDiv"
+      onMouseOver={() => showBrowseBtn(true)}
+      onMouseLeave={() => showBrowseBtn(false)}
+    >
       <div className="collectionImage">
         <span
           className="collectionThumbnail"
           style={{
-            backgroundImage: `url${imageUrl}`,
+            backgroundImage: `url(${imageUrl})`,
             borderBottomRightRadius: "4px",
           }}
         ></span>
         <span
           className="collectionThumbnail"
           style={{
-            backgroundImage: `url${imageUrl}`,
+            backgroundImage: `url(${imageUrl})`,
             borderBottomLeftRadius: "4px",
           }}
         ></span>
         <span
           className="collectionThumbnail"
           style={{
-            backgroundImage: `url${imageUrl}`,
+            backgroundImage: `url(${imageUrl})`,
             borderTopRightRadius: "4px",
           }}
         ></span>
         <span
           className="collectionThumbnail"
           style={{
-            backgroundImage: `url${imageUrl}`,
+            backgroundImage: `url(${imageUrl})`,
             borderTopLeftRadius: "4px",
           }}
         ></span>
@@ -49,6 +59,19 @@ const CollectionCard = ({
         <span className="collectionTitle">{title}</span>
         <span className="collectionCount">{collectionCount}</span>
       </div>
+
+      <AnimatePresence>
+        <motion.span
+          className="browse_btn"
+          initial={{ y: "-100%", opacity: 0 }}
+          animate={
+            browserBtn ? { y: 0, opacity: 1 } : { y: "-100%", opacity: 0 }
+          }
+          exit={{ y: "-100%", opacity: 0 }}
+        >
+          <Button text={"Browse"} icons={<BsStars />} />
+        </motion.span>
+      </AnimatePresence>
     </div>
   );
 };
